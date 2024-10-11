7499f0465bb63eacbddc015fb9c6e75f8a50f6b4
Jon Struve <ocdammo3@gmail.com>
2024-10-11 11:26:19 -0500

    Added initial text inputs for identification - need tests finished before merging

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('app test', () => {
+    test('renders home screen with welcome text and identification fields', () => {
+        render(<App />);
+
+        const idInput = screen.getByLabelText('State Issued ID Number');
+        const last4OfSocialInput = screen.getByLabelText('Last 4 of SSN');
+        expect(idInput).toBeInTheDocument();
+        expect(last4OfSocialInput).toBeInTheDocument();
+    });
+
+    test('id and ssn inputs have tooltips explaining further', () => {
+        render(<App />);
+
+        const idTooltip = screen.getByLabelText('This can be a Driver\'s License Number or State Issued Identification Number.');
+        const last4OfSocialTooltip = screen.getByLabelText('The last 4 (four) digits of your Social Security Number.');
+
+        expect(idTooltip).toBeInTheDocument();
+        expect(last4OfSocialTooltip).toBeInTheDocument();
+    });
 });
